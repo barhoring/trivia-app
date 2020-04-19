d3fe82f86a230901209aef114d4dbbc49a2b8beb
Bar Horing Amir <bar.apple@gmail.com>
2020-04-19 19:49:01 +0300

    save

---
+++ precache-manifest.f483a941b1f78245127a67574c70b878.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a54106ccff856127a6eec58730cb47",
+    "url": "/trivia-app/index.html"
+  },
+  {
+    "revision": "7d13c0bd9988cdef11bb",
+    "url": "/trivia-app/static/css/main.a824d911.chunk.css"
+  },
+  {
+    "revision": "c835d285de80a5025f21",
+    "url": "/trivia-app/static/js/2.d491471e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/trivia-app/static/js/2.d491471e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d13c0bd9988cdef11bb",
+    "url": "/trivia-app/static/js/main.05cb462c.chunk.js"
+  },
+  {
+    "revision": "fbfdc714ab0f4233ac2a",
+    "url": "/trivia-app/static/js/runtime-main.f9639618.js"
+  },
+  {
+    "revision": "f380cb751bcc2e3cfe5bc41f06cb3a30",
+    "url": "/trivia-app/static/media/github.f380cb75.svg"
+  },
+  {
+    "revision": "1c683041c6de2bda5afc9615c47589a0",
+    "url": "/trivia-app/static/media/stackoverflow.1c683041.svg"
+  }
+]);
